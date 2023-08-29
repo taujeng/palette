e6bcaab784c3b55b675b26236cbd52ea0c344f67
Tau <taujeng.1@gmail.com>
2023-08-29 17:20:58 -0600

    Add DailyTask selection, reaction functions, and styling

--- src/app/components/daily/dailyTask/DailyTask.tsx
+++ src/app/components/daily/dailyTask/DailyTask.tsx
@@ -3,29 +3,35 @@
 import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faThumbsDown, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsUp as farThumbsUp, faThumbsDown as farThumbsDown, faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
 import "./dailyTask.css"
 
-const DailyTask = () => {
-  const [like, setLike] = useState(false);
-  const [dislike, setDislike] = useState(false);
-  const [heart, setHeart] = useState(false);
+
+const DailyTask = ( {task} ) => {
+  const [selected, setSelected] = useState(task.selected);
+  const [like, setLike] = useState(task.reaction === "like");
+  const [dislike, setDislike] = useState(task.reaction === "dislike");
+  const [heart, setHeart] = useState(task.reaction === "heart");
   const [likeHover, setLikeHover] = useState(false);
   const [dislikeHover, setDislikeHover] = useState(false);
   const [heartHover, setHeartHover] = useState(false);
   return (
-    <div className="dailyTask-container">
-      school
+    <div className={`dailyTask-container ${selected && "selected"}`} onClick={() => setSelected(!selected)}>
+      {task.name}
       <div className="reaction-container">
-        <button className={`like-btn ${like && "active"} `} onMouseEnter={()=> setLikeHover(true)} onMouseLeave={()=> setLikeHover(false)}>
-          <FontAwesomeIcon icon={faThumbsUp} className={`${likeHover && "fa-bounce"}`}
-            style={{color: "lightgreen", fontSize: 20}}  /></button>
-        <button className={`like-btn ${dislike && "active"}`} onMouseEnter={()=> setDislikeHover(true)} onMouseLeave={()=> setDislikeHover(false)}>
-          <FontAwesomeIcon icon={faThumbsDown} className={`${dislikeHover && "fa-bounce"}`}
-          style={{color: "lightgreen", fontSize: 20}} />
+        <button
+          onClick={(e) => {e.stopPropagation(); setLike(!like); setDislike(false); setHeart(false); setSelected(true);}}
+          onMouseEnter={()=> setLikeHover(true)} onMouseLeave={() => setLikeHover(false)}>
+          <FontAwesomeIcon icon={like ? faThumbsUp : farThumbsUp} className={`reaction-icon ${like && "like"} fa-regular ${likeHover && "fa-bounce"}`}/></button>
+        <button
+          onClick={(e) => {e.stopPropagation(); setDislike(!dislike); setLike(false); setHeart(false); setSelected(true);}}
+          onMouseEnter={()=> setDislikeHover(true)} onMouseLeave={() => setDislikeHover(false)}>
+          <FontAwesomeIcon icon={dislike ? faThumbsDown : farThumbsDown} className={`reaction-icon ${dislike && "dislike"} ${dislikeHover && "fa-bounce"}`} />
         </button>
-        <button className={`like-btn ${heart && "active"}`} onMouseEnter={()=> setHeartHover(true)} onMouseLeave={()=> setHeartHover(false)}>
-          <FontAwesomeIcon icon={faHeart} className={`${heartHover && "fa-bounce"}`}
-          style={{color: "lightgreen", fontSize: 20}} />
+        <button 
+          onClick={(e) => {e.stopPropagation(); setHeart(!heart); setLike(false); setDislike(false); setSelected(true);}}
+          onMouseEnter={()=> setHeartHover(true)} onMouseLeave={() => setHeartHover(false)}>
+          <FontAwesomeIcon icon={heart ? faHeart : farHeart} className={`reaction-icon fa-regular ${heart && "heart"} ${heartHover && "fa-bounce"}`} />
         </button>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-
+'use client'
+import { useState } from "react"
 import DailyTask from "./components/daily/dailyTask/DailyTask"
 
 export default function Home() {
+  const [tasks, setTasks] = useState([
+    {name: "School", selected: true, reaction: "dislike"},
+    {name: "Health", selected: true, reaction: "heart"},
+    {name: "Fitness", selected: true, reaction: "like"},
+    {name: "Games", selected: false, reaction: "none"},
+  
+  ])
   return (
     <main className="">
       <h1>Day</h1>
       <div className="task-container">
-        <DailyTask />
+        {tasks.map((item, i) => <DailyTask key={i} task={item}/>)}
       </div>
     </main>
   )
