f223be420cd3ecf1c71a07a39d1cf79ed130bb84
Tau <taujeng.1@gmail.com>
2023-10-05 16:59:58 +0800

    Add logic for what to display for daySummary page

--- src/app/daySummary/page.tsx
+++ src/app/daySummary/page.tsx
@@ -2,27 +2,83 @@
 
 import React from 'react'
 import { useEntryContext } from '../context/EntryContext'
-import { getDate } from '../utils/dateUtil';
+import { getDate, getFormatDate } from '../utils/dateUtil';
+import "./daySummary.css"
 
 const DaySummary = () => {
   const {state, dispatch} = useEntryContext();
 
+  const summaryDate = getFormatDate();
+
   const entireSummary = state[getDate()]["entries"]
   const selectSummary = entireSummary.filter(entry => {
-    return entry.selected
+    return entry.selected === true
   })
+  if (selectSummary.length < 1) {
+    return <h1>There was a mistake, no items were selected!</h1>
+  }
   const heartSummary = selectSummary.filter(entry => {
     return entry.reaction === "heart"
   })
+  const likeSummary = selectSummary.filter(entry => {
+    return entry.reaction === "like"
+  })
+  const normalSummary = selectSummary.filter(entry => {
+    return entry.reaction === "none"
+  })
+  const dislikeSummary = selectSummary.filter(entry => {
+    return entry.reaction === "dislike"
+  })
+  const badDay = heartSummary.length === 0 && likeSummary.length === 0 && normalSummary.length === 0
 
-  console.log(state[getDate()], selectSummary, heartSummary)
+  console.log(entireSummary, selectSummary, selectSummary, heartSummary)
+  console.log(heartSummary.length, likeSummary.length, normalSummary.length, dislikeSummary.length, badDay);
+
 
   return (
-    <div>
-      {state[getDate()]["entries"].map((x, i) => {
-        return (<h1 key={i}>{x.name}</h1>)
-      })}
-    </div>
+    <main className="daySummary-container">
+      <h1>{summaryDate}</h1>
+      {heartSummary.length > 0 && 
+        <section>
+          <h2>Today's highlights: </h2>
+            <ul>
+              {heartSummary.map((entry,i) => {
+              return <li key={i}>{entry.name}</li>
+              })}
+            </ul>
+        </section>
+      }
+      {likeSummary.length > 0 && 
+        <section>
+          <h2>Honorable Mentions:</h2>
+          <ul>
+            {likeSummary.map((entry, i) => {
+              return <li key={i}>{entry.name}</li>
+            })}
+          </ul>
+        </section>
+      }
+      {normalSummary.length > 0 && 
+        <section>
+          <ul>
+            {normalSummary.map((entry, i) => {
+              return <li key={i}>{entry.name}</li>
+            })} 
+          </ul>
+        </section>
+      }
+      {/* If all of the selected items are disliked..you had a bad day */}
+      {badDay &&
+        <section>
+          <h1>Tomorrow's a new day..</h1>
+          <ul>
+            {dislikeSummary.map((entry, i) => {
+              return <li key={i}>{entry.name}</li>
+            })}
+          </ul>
+        </section>
+      }
+    </main>
   )
 }
 
--- src/app/utils/dateUtil.js
+++ src/app/utils/dateUtil.js
@@ -1,16 +1,29 @@
 const currentDate = new Date();
 
 const getDate = () => {
-  // const currentDate = new Date();
+  // Output: "2023-08-29"
 
   const year = currentDate.getFullYear(); // Gets the current year (e.g., 2023)
   const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Gets the current month (e.g., 08)
   const day = String(currentDate.getDate()).padStart(2, '0'); // Gets the current day of the month (e.g., 29)
 
   const formattedDate = `${year}-${month}-${day}`;
-  // console.log(formattedDate); // Output: "2023-08-29"
 
   return formattedDate;
+}
+
+const getFormatDate = () => {
+  // Output: "March 8"
+  const currentDate = new Date();
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  
+  const month = monthNames[currentDate.getMonth()];
+  const day = currentDate.getDate();
+  
+  return `${month} ${day}`;
 }
 
 const getWeekDay = () => {
@@ -25,4 +38,4 @@ const getWeekDay = () => {
   return dayOfWeekName;
 }
 
-export { getDate, getWeekDay }
+export { getDate, getFormatDate, getWeekDay }
