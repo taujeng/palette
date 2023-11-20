17d99557f7cfc7ce1a8d3bb3dbd092cb7ea498d9
Tau <taujeng.1@gmail.com>
2023-11-20 20:04:21 +0800

    Update data initialization in EntryContext to avoid errors involving localStorage being undefined when the app is being rendered server side

--- src/app/components/weekly/weeklyEntry/WeeklyEntry.tsx
+++ src/app/components/weekly/weeklyEntry/WeeklyEntry.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import './weeklyEntry.css'
 import { useEntryContext } from '@/app/context/EntryContext';
-import { getShortFormatDate } from '@/app/utils/dateUtil';
+import { getShortFormatDate, getWeekDay } from '@/app/utils/dateUtil';
 
 const WeeklyEntry = ( {day} ) => {
-  const { state,dispatch } = useEntryContext();
+  const { state, dispatch } = useEntryContext();
   console.log(state[day])
-  const currentDay = state ? state[day] : "undefined"
-  if (currentDay === "undefined") {
+  const validDay = state ? state[day] : "undefined"
+  if (validDay === "undefined") {
     return <div className="weeklyEntry-container-empty">
       blank
   </div>
   }
+
+  const usableEntries = []
+  const loveEntries = validDay && validDay.entries.filter(entry => entry.reaction === "love")
+  console.log(loveEntries)
+
   return (
     <div className="weeklyEntry-container">
       <header>
         <h1>{getShortFormatDate(new Date(day))}</h1>
-        <h3>{currentDay && currentDay.weekday}</h3>
+        <h3>{getWeekDay(new Date(day))}</h3>
       </header>
       <main>
         content
