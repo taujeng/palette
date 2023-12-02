d7f51bb12696f4e9655b6680b7342a46c3df5d30
Tau <taujeng.1@gmail.com>
2023-12-02 22:16:18 +0800

    Testing out React Quill. Check the Support page for example.

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,7 +1,31 @@
-export default function Support() {
+'use client'
+
+import React, {useState} from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import './support.css'
+
+
+const Support = () => {
+  const [value, setValue] = useState('');
+
+  let quillStyle= {
+    "backgroundColor": "red",
+    padding: 0,
+  }
+
+  let modules = {
+    toolbar: false
+  }
+  console.log(value)
   return (
-    <div className="">
-      <h1>this is the support page</h1>
+    <div>
+      <button onClick={()=> setValue("boomboomboom")}>boom</button>
+      Support Page
+      <ReactQuill style={quillStyle} className="quill-container" placeholder="write here" theme="snow" value={value} modules={modules} onChange={setValue} />
+
     </div>
   )
 }
+
+export default Support;
