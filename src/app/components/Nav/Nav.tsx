f8329114e6ea1fede0a025f0ce18b182846351ac
Tau <taujeng.1@gmail.com>
2023-12-11 17:22:01 +0800

    Mobile view now has a functioning hamburger menu! Also add a _library.scss for color variables.

--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -3,16 +3,30 @@
 import React, {useState} from 'react'
 import Link from 'next/link'
 import './nav.css'
+import './mobileNav/MobileNav'
 import { Cross as Hamburger } from 'hamburger-react'
+
 
 const Nav = () => {
   const [mobileMode, setMobileMode] = useState(false);
   return (
     <header className="nav-container">
       <Link href="/"><img src="images/logo/logo svg 1.svg" alt="my palette logo" /></Link>
-      <Hamburger toggled={mobileMode} toggle={setMobileMode}/>
       <nav>
-        <ul>
+
+        <div className="mobile-nav">
+          <Hamburger toggled={mobileMode} toggle={setMobileMode}></Hamburger>
+          {mobileMode &&
+          <div className="mobile-nav-links">
+            <ul>
+              <li onClick={()=>setMobileMode(false)}><Link href="/day">Journal</Link></li>
+              <li onClick={()=>setMobileMode(false)}><Link href="/whatsNew">What's New</Link></li>
+              <li onClick={()=>setMobileMode(false)}><Link href="/support">Support</Link></li>
+            </ul>
+          </div> 
+          }
+        </div>
+        <ul className="nav-links">
           <li><Link href="/day">Journal</Link></li>
           {/* <li><Link href="/stats">Stats</Link></li> */}
           <li><Link href="/whatsNew">What's New</Link></li>
